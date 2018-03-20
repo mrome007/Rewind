88062ec631d0271580b2900febdd0bff053f3877
mrome007 <mrome007@ucr.edu>
2018-03-19 19:01:28 -0700

    Put rewind functionality in its own class. TODO I'm considering doing this for every object that can rewind.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,32 +11,18 @@ public class PlayerController : MonoBehaviour
 	private float playerSpeed;
 
 	[SerializeField]
-	private int MaxNumberOfCommands;
+	private RewindController rewindController;
 
-	private List<Command> commands;
 	private Vector2 positionVector;
-
-	private int firstCommandIndex;
-	private int lastCommandIndex;
-
-	private bool rewind = false;
 
 	private void Awake()
 	{
-		commands = new List<Command>();
 		positionVector = Vector2.zero;
-
-		Reset();
 	}
 
 	private void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.Return) && !rewind)
-		{
-			StartCoroutine(Rewind());
-		}
-		
-		if(!rewind)
+		if(!rewindController.Rewind)
 		{
 			MovePlayer();
 		}
@@ -47,65 +33,10 @@ public class PlayerController : MonoBehaviour
 		positionVector = player.transform.position;
 		var moveTo = playerSpeed * Time.deltaTime * Vector2.right;
 		positionVector += moveTo;
-		AddMoveCommands(positionVector);
+		var command = new MoveCommand(positionVector.x, positionVector.y, player);
+		rewindController.AddCommands(command);
+		command.Execute();
 	}
 
-	private void AddMoveCommands(Vector2 movePosition)
-	{
-		if(commands.Count < MaxNumberOfCommands) //List is not full.
-		{
-			firstCommandIndex = 0;
-			lastCommandIndex++;
 
-			var command = new MoveCommand(movePosition.x, movePosition.y, player);
-
-			commands.Add(command);
-			command.Execute();
-		}
-		else //List is full.
-		{
-			firstCommandIndex++;
-			firstCommandIndex %= MaxNumberOfCommands;
-
-			lastCommandIndex++;
-			lastCommandIndex %= MaxNumberOfCommands;
-
-			var command = new MoveCommand(movePosition.x, movePosition.y, player);
-
-			commands[lastCommandIndex] = command;
-			command.Execute();
-		}
-	}
-
-	private IEnumerator Rewind()
-	{
-		rewind = true;
-
-		while(lastCommandIndex != firstCommandIndex)
-		{
-			var command = commands[lastCommandIndex];
-			command.Execute();
-
-			yield return null;
-
-			lastCommandIndex--;
-			if(lastCommandIndex < 0)
-			{
-				lastCommandIndex += MaxNumberOfCommands;
-			}
-		}
-
-		Reset();
-		commands.Clear();
-
-		yield return new WaitForSeconds(5f);
-
-		rewind = false;
-	}
-
-	private void Reset()
-	{
-		firstCommandIndex = -1;
-		lastCommandIndex = -1;
-	}
 }
---
+++ Assets/Scripts/RewindController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindController : MonoBehaviour 
+{
+	public bool Rewind { get; private set; }
+	
+	[SerializeField]
+	private int MaxNumberOfCommands;
+	
+	private List<Command> commands;
+	
+	private int firstCommandIndex;
+	private int lastCommandIndex;
+
+	private void Awake()
+	{
+		Rewind = false;
+		commands = new List<Command>();
+		Reset();
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Return) && !Rewind)
+		{
+			StartCoroutine(RewindCommands());
+		}
+	}
+
+	public void AddCommands(Command newCommand)
+	{
+		if(commands.Count < MaxNumberOfCommands) //List is not full.
+		{
+			firstCommandIndex = 0;
+			lastCommandIndex++;
+
+			commands.Add(newCommand);
+		}
+		else //List is full.
+		{
+			firstCommandIndex++;
+			firstCommandIndex %= MaxNumberOfCommands;
+
+			lastCommandIndex++;
+			lastCommandIndex %= MaxNumberOfCommands;
+
+			commands[lastCommandIndex] = newCommand;
+		}
+	}
+
+	private IEnumerator RewindCommands()
+	{
+		Rewind = true;
+
+		while(lastCommandIndex != firstCommandIndex)
+		{
+			var command = commands[lastCommandIndex];
+			command.Execute();
+
+			yield return null;
+
+			lastCommandIndex--;
+			if(lastCommandIndex < 0)
+			{
+				lastCommandIndex += MaxNumberOfCommands;
+			}
+		}
+
+		Reset();
+		commands.Clear();
+
+		yield return new WaitForSeconds(5f);
+
+		Rewind = false;
+	}
+
+	private void Reset()
+	{
+		firstCommandIndex = -1;
+		lastCommandIndex = -1;
+	}
+}
