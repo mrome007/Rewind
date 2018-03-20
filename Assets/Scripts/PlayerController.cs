0861095da43210b035d62577971319342935346a
mrome007 <mrome007@ucr.edu>
2018-03-19 19:11:01 -0700

    Implement accelerated movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,21 @@ public class PlayerController : MonoBehaviour
 	private Player player;
 
 	[SerializeField]
-	private float playerSpeed;
+	private float maxSpeed;
+
+	[SerializeField]
+	private float acceleration;
 
 	[SerializeField]
 	private RewindController rewindController;
 
 	private Vector2 positionVector;
+	private float currentSpeed;
 
 	private void Awake()
 	{
 		positionVector = Vector2.zero;
+		currentSpeed = 0f;
 	}
 
 	private void Update()
@@ -31,12 +36,26 @@ public class PlayerController : MonoBehaviour
 	private void MovePlayer()
 	{
 		positionVector = player.transform.position;
-		var moveTo = playerSpeed * Time.deltaTime * Vector2.right;
+		currentSpeed = GetSpeed(currentSpeed);
+		var moveTo = currentSpeed * Time.deltaTime * Vector2.right;
 		positionVector += moveTo;
 		var command = new MoveCommand(positionVector.x, positionVector.y, player);
 		rewindController.AddCommands(command);
 		command.Execute();
 	}
 
+	private float GetSpeed(float currentSpeed)
+	{
+		var resultSpeed = currentSpeed;
+		if(currentSpeed < maxSpeed)
+		{
+			resultSpeed = resultSpeed + acceleration * Time.deltaTime;
+		}
+		else
+		{
+			resultSpeed = maxSpeed;
+		}
 
+		return resultSpeed;
+	}
 }
