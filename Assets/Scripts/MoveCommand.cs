a83816ec086a1d39a1a933a3e550d583ab50600e
mrome007 <mrome007@ucr.edu>
2018-03-19 18:33:14 -0700

    Basic framework for movement and rewinding the movement using Command Pattern. TODO Figure out a way to lessen instantiating command objects.

---
+++ Assets/Scripts/MoveCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCommand : Command
+{
+	private Player player;
+	private float xPosition;
+	private float yPosition;
+
+	public MoveCommand(float x, float y, Player p)
+	{
+		player = p;
+		xPosition = x;
+		yPosition = y;
+	}
+	
+	public override void Execute()
+	{
+		player.PlayerMove(xPosition, yPosition);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	[SerializeField]
+	private Player player;
+
+	[SerializeField]
+	private float playerSpeed;
+
+	[SerializeField]
+	private int MaxNumberOfCommands;
+
+	private List<Command> commands;
+	private Vector2 positionVector;
+
+	private int firstCommandIndex;
+	private int lastCommandIndex;
+
+	private bool rewind = false;
+
+	private void Awake()
+	{
+		commands = new List<Command>();
+		positionVector = Vector2.zero;
+
+		Reset();
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Return) && !rewind)
+		{
+			StartCoroutine(Rewind());
+		}
+		
+		if(!rewind)
+		{
+			MovePlayer();
+		}
+	}
+
+	private void MovePlayer()
+	{
+		positionVector = player.transform.position;
+		var moveTo = playerSpeed * Time.deltaTime * Vector2.right;
+		positionVector += moveTo;
+		AddMoveCommands(positionVector);
+	}
+
+	private void AddMoveCommands(Vector2 movePosition)
+	{
+		if(commands.Count < MaxNumberOfCommands) //List is not full.
+		{
+			firstCommandIndex = 0;
+			lastCommandIndex++;
+
+			var command = new MoveCommand(movePosition.x, movePosition.y, player);
+
+			commands.Add(command);
+			command.Execute();
+		}
+		else //List is full.
+		{
+			firstCommandIndex++;
+			firstCommandIndex %= MaxNumberOfCommands;
+
+			lastCommandIndex++;
+			lastCommandIndex %= MaxNumberOfCommands;
+
+			var command = new MoveCommand(movePosition.x, movePosition.y, player);
+
+			commands[lastCommandIndex] = command;
+			command.Execute();
+		}
+	}
+
+	private IEnumerator Rewind()
+	{
+		rewind = true;
+
+		while(lastCommandIndex != firstCommandIndex)
+		{
+			var command = commands[lastCommandIndex];
+			command.Execute();
+
+			yield return null;
+
+			lastCommandIndex--;
+			if(lastCommandIndex < 0)
+			{
+				lastCommandIndex += MaxNumberOfCommands;
+			}
+		}
+
+		Reset();
+		commands.Clear();
+
+		yield return new WaitForSeconds(5f);
+
+		rewind = false;
+	}
+
+	private void Reset()
+	{
+		firstCommandIndex = -1;
+		lastCommandIndex = -1;
+	}
+}
