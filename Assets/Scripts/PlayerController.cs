bebe7c57055f00d090db06870d2f26f95e9d0347
mrome007 <mrome007@ucr.edu>
2018-03-19 19:35:44 -0700

    Basic jump. TODO make jump routine checks based on y position instead of number of frames so I can resume from jumping or continue jumping after rewinding.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,48 +14,110 @@ public class PlayerController : MonoBehaviour
 	private float acceleration;
 
 	[SerializeField]
+	private float maxJumpSpeed;
+
+	[SerializeField]
+	private float jumpAcceleration;
+
+	[SerializeField]
 	private RewindController rewindController;
 
 	private Vector2 positionVector;
 	private float currentSpeed;
+	private float currentJumpSpeed;
+	private bool jumping;
+	private Coroutine jumpRoutine = null;
 
 	private void Awake()
 	{
 		positionVector = Vector2.zero;
 		currentSpeed = 0f;
+		currentJumpSpeed = 0f;
+		jumping = false;
 	}
 
 	private void Update()
 	{
-		if(!rewindController.Rewind)
+		if(rewindController.Rewind)
 		{
-			MovePlayer();
+			if(jumpRoutine != null)
+			{
+				StopCoroutine(jumpRoutine);
+				jumpRoutine = null;
+				jumping = false;
+			}
+			return;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Space) && !jumping)
+		{
+			if(jumpRoutine == null)
+			{
+				jumpRoutine = StartCoroutine(Jump());
+			}
+		}
+
+		if(!jumping)
+		{
+			MovePlayer(Vector2.right);
 		}
 	}
 
-	private void MovePlayer()
+	private float GetSpeed(float curSpd, float acc, float maxVel)
+	{
+		var resultSpeed = curSpd;
+		if(curSpd < maxVel)
+		{
+			resultSpeed = resultSpeed + acc * Time.deltaTime;
+		}
+		else
+		{
+			resultSpeed = maxVel;
+		}
+
+		return resultSpeed;
+	}
+
+	private IEnumerator Jump()
+	{
+		jumping = true;
+		var count = 0;
+		currentJumpSpeed = 0f;
+
+		while(count < 30)
+		{
+			MovePlayer(Vector2.one);
+			yield return null;
+			count++;
+		}
+
+		count = 0;
+		currentJumpSpeed = 0f;
+
+		while(count < 30)
+		{
+			MovePlayer(new Vector2(1f, -1f));
+			yield return null;
+			count++;
+		}
+
+		jumping = false;
+		jumpRoutine = null;
+	}
+
+	private void MovePlayer(Vector2 direction)
 	{
 		positionVector = player.transform.position;
-		currentSpeed = GetSpeed(currentSpeed);
-		var moveTo = currentSpeed * Time.deltaTime * Vector2.right;
+		currentSpeed = GetSpeed(currentSpeed, acceleration, maxSpeed);
+		currentJumpSpeed = GetSpeed(currentJumpSpeed, jumpAcceleration, maxJumpSpeed);
+		var moveX = currentSpeed * Time.deltaTime;
+		var moveY = currentJumpSpeed * Time.deltaTime;
+		var moveTo = direction;
+		moveTo.x *= moveX;
+		moveTo.y *= moveY;
 		positionVector += moveTo;
 		var command = new MoveCommand(positionVector.x, positionVector.y, player);
 		rewindController.AddCommands(command);
 		command.Execute();
-	}
-
-	private float GetSpeed(float currentSpeed)
-	{
-		var resultSpeed = currentSpeed;
-		if(currentSpeed < maxSpeed)
-		{
-			resultSpeed = resultSpeed + acceleration * Time.deltaTime;
-		}
-		else
-		{
-			resultSpeed = maxSpeed;
-		}
-
-		return resultSpeed;
 	}
 }
