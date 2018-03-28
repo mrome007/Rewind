1a32a405fb7c217f8009f71e3b6515b82ee0aaed
mrome007 <mrome007@ucr.edu>
2018-03-27 21:40:05 -0700

    Add references to move tile directions. This is to setup changing directions at run time.

--- Assets/Scripts/MoveTile.cs
+++ Assets/Scripts/MoveTile.cs
@@ -10,6 +10,53 @@ public class MoveTile : MonoBehaviour
     [SerializeField]
     private MoveTile previousTile;
 
+    [SerializeField]
+    private MoveTile Up;
+
+    [SerializeField]
+    private MoveTile Down;
+
+    [SerializeField]
+    private MoveTile Left;
+
+    [SerializeField]
+    private MoveTile Right;
+
+    public enum TileDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
     public MoveTile NextTile { get { return nextTile; } }
     public MoveTile PreviousTile { get { return previousTile; } }
+
+    public void ChangeNextTile(TileDirection direction)
+    {
+        switch(direction)
+        {
+            case TileDirection.Up:
+                nextTile = Up;
+                break;
+
+            case TileDirection.Down:
+                nextTile = Down;
+                break;
+
+            case TileDirection.Left:
+                nextTile = Left;
+                break;
+
+            case TileDirection.Right:
+                nextTile = Right;
+                break;
+
+            default:
+                break;
+        }
+    }
 }
+
+
